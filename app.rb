4928a9c3d97f33bac94350d587632730645e24e8
Anderson Dias <andersondaraujo@gmail.com>
2017-08-16 17:15:59 -0300

    Validating congratulations messages
    We'll validate if the given text is a help/empty message and return a usage hint, in other case we will try to validate the congratulate command expecting "@username text to user" pattern.
    
    If it does not matches any of these actions we will return an invalid command message.

--- app.rb
+++ app.rb
@@ -4,10 +4,22 @@ require_relative 'app/slack_authorizer'
 
 use SlackAuthorizer
 
+VALID_CONGRATULATE_EXPRESSION = /^(@[\w\.\-_]+) (.+)/
+
 HELP_RESPONSE = 'Use `/congratulate` to send a congratulation message to '\
-                'someone. Example:\n* `/congratulate @anderson for design the '\
+                'someone. Example: `/congratulate @anderson for design the '\
                 'new API`'.freeze
 
+OK_RESPONSE = "Thanks for sending this! I'll share it with %s.".freeze
+
+INVALID_RESPONSE = 'Sorry, I didn’t quite get that. Perhaps try the words in a'\
+                   ' different order? This usually works: '\
+                   '`/congratulate [@someone] [message]`.'.freeze
+
 post '/slack/command' do
-  HELP_RESPONSE
+  case params['text'].to_s.strip
+  when 'help', '' then HELP_RESPONSE
+  when VALID_CONGRATULATE_EXPRESSION then OK_RESPONSE % $1
+  else INVALID_RESPONSE
+  end
 end
--- test/app_test.rb
+++ test/app_test.rb
@@ -53,4 +53,36 @@ class AppTest < Test::Unit::TestCase
     assert_equal 200, last_response.status
     assert_equal HELP_RESPONSE, last_response.body
   end
+
+  def test_it_returns_200_with_success_message_if_valid
+    post '/slack/command', token: '123',
+                           command: '/congratulation',
+                           text: '@anderson for delivering this api!',
+                           user_name: 'matz'
+
+    assert_equal 200, last_response.status
+
+    success_message = "Thanks for sending this! I'll share it with @anderson."
+    assert_equal success_message, last_response.body
+  end
+
+  def test_it_returns_200_with_text_sample_if_missing_to
+    post '/slack/command', token: '123',
+                           command: '/congratulation',
+                           text: 'for delivering this api!',
+                           user_name: 'matz'
+
+    assert_equal 200, last_response.status
+    assert_equal INVALID_RESPONSE, last_response.body
+  end
+
+  def test_it_returns_200_with_text_sample_if_missing_message
+    post '/slack/command', token: '123',
+                           command: '/congratulation',
+                           text: '@anderson',
+                           user_name: 'matz'
+
+    assert_equal 200, last_response.status
+    assert_equal INVALID_RESPONSE, last_response.body
+  end
 end
